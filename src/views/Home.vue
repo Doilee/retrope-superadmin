786c5b60ae04a103d4b42612bb021dc289e6dc0c
arbak <l.arbaczewski@samhoud.com>
2018-10-23 15:59:59 +0200

    fixed parsing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Login page of Superadmin.Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
